1f02fc3eb649b5e57a4e75de7f2c5aa32c423648
Mukul <budhirajasmukul8@gmail.com>
2026-06-25 11:31:00 +0530

    not-found-quotation-route

--- app/(erp)/quotations/[quotationId]/page.tsx
+++ app/(erp)/quotations/[quotationId]/page.tsx
@@ -1,6 +1,23 @@
+// import { QuotationDetailPage } from "@/modules/quotation/components/quotation-detail-page";
+
+// export default async function QuotationDetailRoute({ params }: { params: Promise<{ quotationId: string }> }) {
+//   const { quotationId } = await params;
+//   return <QuotationDetailPage quotationId={quotationId} />;
+// }
+
+import { redirect } from "next/navigation";
 import { QuotationDetailPage } from "@/modules/quotation/components/quotation-detail-page";
 
-export default async function QuotationDetailRoute({ params }: { params: Promise<{ quotationId: string }> }) {
+export default async function QuotationDetailRoute({
+  params,
+}: {
+  params: Promise<{ quotationId: string }>;
+}) {
   const { quotationId } = await params;
+  const isValidId = /^[0-9a-fA-F]{24}$/.test(quotationId);
+
+  if (!isValidId) {
+    redirect("https://www.glazia.in");
+  }
   return <QuotationDetailPage quotationId={quotationId} />;
 }
